947294f29c661e4b22dfed0f20e1197f5c7374bf
Frist <Frist@DESKTOP-3A1RSJK.station>
2023-04-22 13:43:53 +0100

    encrypt complete

--- softwareSecurityCA/src/main/java/shutup/softwaresecurityca/encrypt.java
+++ softwareSecurityCA/src/main/java/shutup/softwaresecurityca/encrypt.java
@@ -4,48 +4,81 @@
  */
 package shutup.softwaresecurityca;
 
+import java.io.File;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
-/**
- *
- * @author Frist
- */
 public class encrypt {
-    public static SecretKey generateKey1(int Key) throws NoSuchAlgorithmException{
+
+    /**
+     * Generates a new AES encryption key with the specified key size.
+     *
+     * @param keySize The size of the key to be generated, in bits.
+     * @return The generated encryption key.
+     * @throws NoSuchAlgorithmException if the AES encryption algorithm is not
+     * available.
+     */
+    public static SecretKey generateKey1(int keySize) throws NoSuchAlgorithmException {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(Key);
+        keyGenerator.init(keySize);
         SecretKey encryptionKey = keyGenerator.generateKey();
         return encryptionKey;
-       
     }
-    
+
+    /**
+     * Encrypts the given message using the specified key.
+     *
+     * @param message The message to be encrypted.
+     * @param key The key to be used for encryption.
+     * @return The encrypted message as a byte array.
+     *
+     */
+    public static byte[] encrypt(String message, SecretKey key) throws Exception {
+        SecretKeySpec secretKey2 = new SecretKeySpec(key.getEncoded(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey2);
+        return cipher.doFinal(message.getBytes());
+    }
+
+    /**
+     * Encrypts the given message using the specified key string.
+     *
+     * @param message The message to be encrypted.
+     * @param key The key string to be used for encryption.
+     * @return The encrypted message as a byte array.
+     *
+     */
     public static byte[] encryptWithKey(String message, String key) throws Exception {
         SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        
         return cipher.doFinal(message.getBytes());
     }
-    
-    public static byte[] encrypt(String message, SecretKey key) throws Exception{
-        SecretKeySpec secretKey2 = new SecretKeySpec(key.getEncoded(),"AES");
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey2);
-        
-        return cipher.doFinal(message.getBytes());
+
+    /**
+     *
+     * @param algorithm The name of the encryption algorithm.
+     * @return The maximum allowed key size for the specified encryption
+     * algorithm, in bits.
+     * @throws NoSuchAlgorithmException if the specified encryption algorithm is
+     * not available.
+     */
+    public static int maxKeySize(String algorithm) throws NoSuchAlgorithmException {
+        return Cipher.getMaxAllowedKeyLength(algorithm);
     }
-    public static int maxKeySize(String message) throws NoSuchAlgorithmException{
-        return Cipher.getMaxAllowedKeyLength(message);
-    }
-    public static int maxKeySizeAES() throws NoSuchAlgorithmException{
+
+    /**
+     *
+     * @return The maximum allowed key size for the AES encryption algorithm, in
+     * bits.
+     * @throws NoSuchAlgorithmException if the AES encryption algorithm is not
+     * available.
+     */
+    public static int maxKeySizeAES() throws NoSuchAlgorithmException {
         return Cipher.getMaxAllowedKeyLength("AES");
     }
-   
-    }
-   
-  
 
+}
