177905fbeaf1b89653712819a8ad3269a9fc777a
Frist <Frist@DESKTOP-3A1RSJK.station>
2023-04-22 13:43:07 +0100

    menu complete

--- softwareSecurityCA/src/main/java/shutup/softwaresecurityca/Menu.java
+++ softwareSecurityCA/src/main/java/shutup/softwaresecurityca/Menu.java
@@ -11,6 +11,10 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 import javax.crypto.SecretKey;
+import static shutup.softwaresecurityca.decrypt.decryptGivenKey;
+import static shutup.softwaresecurityca.decrypt.decryptRandomKey;
+import static shutup.softwaresecurityca.decrypt.readFileToString;
+import static shutup.softwaresecurityca.encrypt.encrypt;
 import static shutup.softwaresecurityca.encrypt.encryptWithKey;
 import static shutup.softwaresecurityca.encrypt.generateKey1;
 
@@ -34,32 +38,46 @@ public class Menu {
             String answer2 = sc.nextLine();
             File file = new File(answer2 + ".txt");
             FileWriter fw = new FileWriter(file);
-            FileReader fr=new FileReader(file);   
+            FileReader fr = new FileReader(file);
             System.out.println("What do you wish to add to the file ?");
-           String answer3 = sc.nextLine();
+            String answer3 = sc.nextLine();
 
-    fw.write(answer3);
-    fw.flush();
-    fw.close();
+            fw.write(answer3);
+            fw.flush();
+            fw.close();
 
-            
             //now that file has been create encrypt it
             System.out.println("your file will now be encrypted, do you want to use your own key for encryption? if no the program will generate a random one");
             String answer4 = sc.nextLine();
-            if(answer4.equalsIgnoreCase("Yes")){
+            if (answer4.equalsIgnoreCase("Yes")) {
                 System.out.println("What do you want your key to be, keep in mind the only possible keysizes are 1 byte(32 characters long) or half of 1 byte(16characters long)");
-            String keyPersonal = sc.nextLine();
-            String message = fr.toString();
-           byte[] encrypted = encryptWithKey(message, keyPersonal);
-           System.out.println("Encrypted message: " + new String(encrypted));
-            }
-            else{
+                String keyPersonal = sc.nextLine();
+                String message = readFileToString(file);
+                byte[] encrypted = encryptWithKey(message, keyPersonal);
+                System.out.println("Encrypted message: " + new String(encrypted));
+                //now decrypt
+                System.out.println("do you wish to decrypt this file? Y/N");
+                String answer5 = sc.nextLine();
+                if (answer5.equalsIgnoreCase("Yes")) {
+                    String decrypted = decryptGivenKey(encrypted, keyPersonal);
+                    System.out.println("Decrypted message: " + decrypted);
+                }
+            } else {
                 SecretKey secretKey = generateKey1(keySize);
-                
-                
+                String message = readFileToString(file);
+               
+                    byte[] encrypted = encrypt(message, secretKey);
+                System.out.println("Encrypted message: " + new String(encrypted));
+                System.out.println("do you wish to decrypt this file? Y/N");
+                String answer5 = sc.nextLine();
+                if (answer5.equalsIgnoreCase("Yes")) {
+                    String decrypted = decryptRandomKey(encrypted, secretKey);
+                    System.out.println("Decrypted message: " + decrypted);
+                    
             }
-            
+
         }
     }
-    
+
+}
 }
